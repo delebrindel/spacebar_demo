5f8871f08c32acaf9cb63cdf77884e14e8634e76
Cristóbal Torres <delebrindel@gmail.com>
2019-04-05 09:25:00 -0600

    Changed how slides work

--- public/slides/slide_5.php
+++
@@ -1,4 +0,0 @@
-<section id="Slide_5">
-    <h1 class="Title has-text-link">Actual example</h1>
-    <img src="img/990.png" alt="Bug ticket">
-</section>
--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 var Reveal = require('reveal');
+var $ = require('jquery');
 // Full list of configuration options available here:
 // https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-  controls: true,
-  progress: true,
-  history: true,
-  center: true,
-  // default/cube/page/concave/zoom/linear/fade/none
-  transition: 'none',
+
+$(function(){
+  let slidesFolder ="slides/";
+  $("#slide-1").load( slidesFolder + "slide_1.html"); 
+  $("#slide-2").load( slidesFolder + "slide_2.html"); 
+  $("#slide-3").load( slidesFolder + "slide_3.html"); 
+  $("#slide-4").load( slidesFolder + "slide_4.html"); 
+  $("#slide-5").load( slidesFolder + "slide_5.html"); 
+  $("#slide-6").load( slidesFolder + "slide_6.html"); 
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    // default/cube/page/concave/zoom/linear/fade/none
+    transition: 'none',
+  });
 });
